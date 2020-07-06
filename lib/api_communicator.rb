a790d6bb94241ac6df715815fc087a9b458c5917
matthewlawrenceklein <mtt.l.kln@gmail.com>
2020-07-06 13:42:57 -0500

    did it

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,6 +1,7 @@
 require 'rest-client'
 require 'json'
 require 'pry'
+require 'awesome_print'
 
 def get_character_movies_from_api(character_name)
   #make the web request
@@ -9,27 +10,35 @@ def get_character_movies_from_api(character_name)
   response_string = RestClient.get('http://swapi.dev/api/people')
   response_hash = JSON.parse(response_string)
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+  character_data = response_hash["results"].find { |data| data["name"] == character_name }
+  film_urls = character_data["films"]
+  film_data = film_urls.collect { |data| JSON.parse(RestClient.get(data)) }
+  
+  
+
 end
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+  films.each do |data|
+    ap data["title"]
+  end
+end
+
+def print_producer(films)
+  films.each do |data|
+    ap data["producer"]
+  end
 end
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
   print_movies(films)
+  print_producer(films)
 end
 
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+
+
